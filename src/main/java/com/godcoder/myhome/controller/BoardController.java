caa7729697e3001dfdac2e59d8fc0f9be643503d
Chanmin Kim <power2rapcm@gmail.com>
2021-08-18 20:03:45 +0900

    REST API POST with update function

--- src/main/java/com/godcoder/myhome/controller/BoardController.java
+++ src/main/java/com/godcoder/myhome/controller/BoardController.java
@@ -2,13 +2,13 @@ package com.godcoder.myhome.controller;
 
 import com.godcoder.myhome.model.Board;
 import com.godcoder.myhome.repository.BoardRepository;
+import com.godcoder.myhome.validator.BoardValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,6 +19,9 @@ public class BoardController {
     @Autowired
     private BoardRepository boardRepository;
 
+    @Autowired
+    private BoardValidator boardValidator;
+
     @GetMapping("/list")
     public String list(Model model) {
         List<Board> boards = boardRepository.findAll();
@@ -27,13 +30,22 @@ public class BoardController {
     }
 
     @GetMapping("/form")
-    public String form(Model model) {
-        model.addAttribute("board", new Board());
+    public String form(Model model, @RequestParam(required = false) Long id) {
+        if(id == null) {
+            model.addAttribute("board", new Board());
+        } else {
+            Board board = boardRepository.findById(id).orElse(null);
+            model.addAttribute("board", board);
+        }
         return "board/form";
     }
 
     @PostMapping("/form")
-    public String boardSubmit(@ModelAttribute Board board) {
+    public String boardSubmit(@Validated Board board, BindingResult bindingResult) {
+        boardValidator.validate(board, bindingResult);
+        if (bindingResult.hasErrors()) {
+            return "board/form";
+        }
         boardRepository.save(board);
         return "redirect:/board/list";
     }
